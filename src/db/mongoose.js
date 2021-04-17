0eb66ede1e0fc89cc8ac4ce28dcd0e73f9482e27
pranav1601 <pranav.agarw@gmail.com>
2021-04-17 11:03:00 +0530

    added rest api connection endpoint for tasks, reformated code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,15 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-application-api',{
     useNewUrlParser:true,
     useCreateIndex:true
 })
-
-const Task=mongoose.model('Tasks',{
-    description:{
-        type:String,
-        trim:true,
-        required:true
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    }
-})
