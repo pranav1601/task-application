967af88f5af58376fb827d80fed55ed6d64d7941
pranav1601 <pranav.agarw@gmail.com>
2021-04-19 06:58:19 +0530

    added fucntionality to delete task

--- src/index.js
+++ src/index.js
@@ -136,6 +136,22 @@ app.patch('/tasks/:id',async(req,res)=>{
     }
 })
 
+app.delete('/tasks/:id',async(req,res)=>{
+    
+
+    try {
+        const task= await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+    
+        res.status(200).send(task)
+    } catch (error) {
+        res.status(500).send()
+    }
+    
+})
+
 app.listen(port,()=>{
     console.log('server is up on port '+port)
 })
