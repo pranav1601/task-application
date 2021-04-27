15932e6d72f0de78afb27330eda1b4d4ebddb4dc
pranav1601 <pranav.agarw@gmail.com>
2021-04-27 06:48:20 +0530

    added test using jest for express functioning

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-const express=require('express')
-require('./db/mongoose')
-const User=require('./models/user')
-const Task=require('./models/task')
-const userRouter=require('./routers/user')
-const taskRouter=require('./routers/task')
+const app=require('./app')
 
-const app=express()
-const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+port=process.env.PORT
 
 app.listen(port,()=>{
     console.log('server is up on port '+port)
