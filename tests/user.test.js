98cf142649587f604cd0be6ed62dd325732ec69d
pranav1601 <pranav.agarw@gmail.com>
2021-04-28 20:10:50 +0530

    added test for login existing user and should not login existing user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request=require('supertest')
 const app=require('../src/app')
+const User=require('../src/models/user')
+
+const userOne={
+    name:'Mike',
+    email:'mike@example.com',
+    password:'thisismike'
+}
+
+beforeEach(async()=>{
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should signup a new user',async()=>{
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should signup a new user',async()=>{
         email:'pranav@gmail.com',
         password:'pranavagarwal'
     }).expect(201)
+})
+
+test('Should login existing user',async()=>{
+    await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:userOne.password
+    }).expect(200)
+})
+
+test('Should not login existing user',async()=>{
+    await request(app).post('/users/login').send({
+        email:'hello',
+        password:'helloitsme'
+    }).expect(400)
 })
