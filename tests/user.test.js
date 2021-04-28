bf63655d2c7bbe5df191da5f4262d81715fc8a15
pranav1601 <pranav.agarw@gmail.com>
2021-04-28 20:43:40 +0530

    added test for authoriation to view profile and delete profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,18 @@
 const request=require('supertest')
+const jwt=require('jsonwebtoken')
+const mongoose=require('mongoose')
 const app=require('../src/app')
 const User=require('../src/models/user')
 
+const userOneId=new mongoose.Types.ObjectId()
 const userOne={
+    _id:userOneId,
     name:'Mike',
     email:'mike@example.com',
-    password:'thisismike'
+    password:'thisismike',
+    tokens:[{
+        token:jwt.sign({_id:userOneId},process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async()=>{
@@ -33,4 +40,20 @@ test('Should not login existing user',async()=>{
         email:'hello',
         password:'helloitsme'
     }).expect(400)
+})
+
+test('should get profile for user',async()=>{
+    await request(app).get('/users/me').set('Authorization',`Bearer ${userOne.tokens[0].token}`).send().expect(200)
+})
+
+test('should not get profile for unauthenticated user',async()=>{
+    await request(app).get('/users/me').send().expect(401)
+})
+
+test('should delete account for user',async()=>{
+    await request(app).delete('/users/me').set('Authorization',`Bearer ${userOne.tokens[0].token}`).send().expect(200)
+})
+
+test('should delete account for user',async()=>{
+    await request(app).delete('/users/me').send().expect(401)
 })
